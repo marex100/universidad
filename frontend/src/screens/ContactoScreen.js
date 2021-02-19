e1f2c688403a61bd918ae0688db8acf746d550f0
marex100 <marioandres107@gmail.com>
2021-02-19 16:59:36 -0600

    2/19/2021 5:00 pm todo bien hasta este checkpoint

--- frontend/src/screens/ContactoScreen.js
+++ frontend/src/screens/ContactoScreen.js
@@ -13,13 +13,13 @@ const ContactoScreen = {
       <a href="https://api.whatsapp.com/send?phone=50685224085" class="anch" target="_blank" rel="noopener noreferrer"><i class="fa fa-whatsapp fa-5x" aria-hidden="true"></i></a>
       <a href="https://www.facebook.com/llantasgrecia" class="anch" target="_blank" rel="noopener noreferrer"><i class="fa fa-facebook fa-5x"></i></a>
 
-      <a href="#" class="anch"><i class="fas fa-map-marker-alt fa-5x " aria-hidden="true"></i>	</a>
+      <a href="https://goo.gl/maps/LkHFGgeR19E669DV6" class="anch" target="_blank" ><i class="fas fa-map-marker-alt fa-5x " aria-hidden="true"></i>	</a>
 
-      <a href="#" class="anch"><i class="fab fa-waze fa-5x"  aria-hidden="true"></i></a>
+      <a href="https://ul.waze.com/ul?preview_venue_id=180682853.1806762994.3866329&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location" class="anch" target="_blank"><i class="fab fa-waze fa-5x"  aria-hidden="true"></i></a>
 
 
 
-      <a href="#"  class="anch"> <i class="fa fa-instagram fa-5x" aria-hidden="true"></i></a>
+      <a href="https://www.instagram.com/llantasgrecia/?hl=es-la"  class="anch" target="_blank"> <i class="fa fa-instagram fa-5x" aria-hidden="true"></i></a>
 
   </div>      
       
